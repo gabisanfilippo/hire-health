31de4b72869cae88174835e459af8bb244a67f39
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-04-29 17:17:11 -0300

    chore: config for mock image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "web.whatsapp.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
