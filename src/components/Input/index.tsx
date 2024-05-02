ced3e876aa2d1353ebf1647b2cc12a319775b39f
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-02 17:48:26 -0300

    feat: input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,48 @@
+import { RegisterInputs } from "../FormRegister/schema";
+import { ChangeEvent, HTMLInputTypeAttribute } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type IProps = {
+  register?: UseFormRegisterReturn<keyof RegisterInputs>;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  type: HTMLInputTypeAttribute;
+  errorMessage?: string;
+  name: string;
+  label?: string;
+  className?: string;
+};
+
+export const Input = ({
+  onChange,
+  register,
+  type,
+  errorMessage,
+  name,
+  label,
+  className,
+}: IProps) => {
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <label
+        htmlFor={name}
+        className={`${
+          errorMessage ? "text-custom-pink" : "text-custom-blue"
+        } text-sm font-semibold`}
+      >
+        {label}
+      </label>
+      <input
+        name={name}
+        type={type}
+        className={`border-2 ${
+          errorMessage ? "border-custom-pink" : "border-gray-400"
+        } rounded-lg px-4 py-2 bg-white focus:border-custom-blue outline-none`}
+        {...register}
+        onChange={onChange}
+      />
+      <span className="min-h-6 text-custom-pink font-semibold">
+        {errorMessage}
+      </span>
+    </div>
+  );
+};
