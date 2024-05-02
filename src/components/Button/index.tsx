bbecf05f8315e36087ee634061821b5df68bdb68
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-02 17:49:26 -0300

    feat: adding type prop and more classes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,18 @@
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 
 interface IProps {
   content: ReactNode;
   onClick?: () => void;
+  type?: "submit" | "reset";
+  className?: string;
 }
 
-export const Button = ({ content, onClick }: IProps) => {
+export const Button = ({ content, onClick, type, className }: IProps) => {
   return (
     <button
-      className="bg-custom-pink px-4 py-2 rounded-lg text-white font-medium transition-all"
+      className={`bg-custom-pink px-4 py-2 rounded-lg text-white font-medium transition-all ${className}`}
       onClick={onClick}
+      type={type || "button"}
     >
       {content}
     </button>
