20a13bdc118985b45689325256d1e44fcdd76b33
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-03 15:12:42 -0300

    feat: size prop

--- src/assets/icons/IconProfile/index.tsx
+++ src/assets/icons/IconProfile/index.tsx
@@ -1,8 +1,8 @@
-export const IconProfile = () => {
+export const IconProfile = ({ size = 80 }: { size?: number }) => {
   return (
     <svg
-      width="80"
-      height="80"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
