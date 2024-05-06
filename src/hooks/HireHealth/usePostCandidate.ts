f3b8c3399deb6e14ee2273171b19143138d75d4d
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-06 10:05:11 -0300

    feat: invalidating get on post

--- src/hooks/HireHealth/usePostCandidate.ts
+++ src/hooks/HireHealth/usePostCandidate.ts
@@ -1,10 +1,17 @@
 import { ApiHireHealth } from "@/services/HireHealth";
 import { RegisterInputs } from "@/types/registerForm";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const usePostCandidate = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ["POST_CANDIDATE"],
     mutationFn: (body: RegisterInputs) => ApiHireHealth.postCandidate(body),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["GET_CANDIDATES"],
+        exact: true,
+      }),
   });
 };
