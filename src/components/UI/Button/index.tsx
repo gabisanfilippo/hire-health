e9ac8f8fe0c3c78e8720fef51549116f60b51f68
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-06 09:57:33 -0300

    feat: loading

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,3 +1,4 @@
+import { GifLoading } from "@/assets/gifs";
 import { HTMLAttributes, ReactNode } from "react";
 
 interface IProps {
@@ -5,15 +6,30 @@ interface IProps {
   onClick?: () => void;
   type?: "submit" | "reset";
   className?: string;
+  isDisabled?: boolean;
+  isLoading?: boolean;
 }
 
-export const Button = ({ content, onClick, type, className }: IProps) => {
+export const Button = ({
+  content,
+  onClick,
+  type,
+  className,
+  isDisabled = false,
+  isLoading = false,
+}: IProps) => {
   return (
     <button
-      className={`bg-custom-pink px-4 py-2 rounded-lg text-white font-medium transition-all ${className}`}
+      className={`bg-custom-pink px-4 py-2 rounded-lg text-white font-medium transition-all flex items-center ${
+        isLoading || isDisabled
+          ? "opacity-50 hover:opacity-50"
+          : "opacity-100 hover:opacity-100"
+      } ${className}`}
       onClick={onClick}
       type={type || "button"}
+      disabled={isDisabled || isLoading}
     >
+      {isLoading && <GifLoading />}
       {content}
     </button>
   );
