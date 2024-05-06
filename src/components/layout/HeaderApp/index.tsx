9bca49a78facb0fc203fd871515a3e4dbbd3f0ab
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-06 18:16:22 -0300

    feat: redirect to home

--- src/components/layout/HeaderApp/index.tsx
+++ src/components/layout/HeaderApp/index.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { IconLogo, IconProfileDefault } from "@/assets/icons";
+import { useRouter } from "next/navigation";
 
 export const HeaderApp = () => {
+  const router = useRouter();
+
   return (
     <header className="h-20 max-w-screen-xl m-auto py-4 px-8 flex items-center justify-between gap-4 sticky top-0 backdrop-blur-lg z-30 mb-8">
-      <div className="flex items-center gap-4">
+      <div
+        className="flex items-center gap-4 cursor-pointer"
+        onClick={() => router.push("/")}
+      >
         <IconLogo />
         <h1 className="text-xl font-bold w-max text-custom-blue">
           Hire Health
